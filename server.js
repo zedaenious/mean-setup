584c466be2b33ae438780ffa2ba7d516cb2fdf2a
zedaenious <zed.dev.22@gmail.com>
2022-12-28 12:46:20 -0500

    trimming deprecated mongoose options from server.js

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedToplogy: true
-    })
+    .connect(db.url, {})
     .then(console.log('Connected to the database'))
     .catch(err => {
         console.log('Cannot connect to the database', err);
